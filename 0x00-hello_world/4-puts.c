b72be77580744ac89fdaa64fe1ae24a53845af38
Kolagolden <abdullahganiyu31@gmail.com>
2022-09-08 15:10:50 -0700

    Comit files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
  * main - Entry point
  *
  * Description : using main function
- * Return:  0 
+ * Return: 0 
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
