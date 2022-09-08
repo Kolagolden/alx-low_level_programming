01e8d971d9de0b955b1aea4ef09d3389e4a86cc3
Kolagolden <abdullahganiyu31@gmail.com>
2022-09-08 15:16:43 -0700

    Commit files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+    * main - Entry point
+    * Description : using the main function
+    * Return: Always 0 (Success)
+    */
 int main(void)
 
 {
