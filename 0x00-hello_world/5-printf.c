235b12c5c6478443f0fd2dd5237fdb6456327a38
Kolagolden <abdullahganiyu31@gmail.com>
2022-09-08 14:46:22 -0700

    Commit files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 /*
- *  * main - Entry point
+ *  main - Entry point
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *  Description : using main function
+ *  Return: 0 
+      */
 int main(void)
 {
-		printf("with proper grammar, but the a piece of art, \n");
+		printf("with proper grammar, but the outcome is a piece of art, \n");
 			return (0);
 }
